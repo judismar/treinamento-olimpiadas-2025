ccf00d4969fee75b4ba9c19cd4baf1570d5b2f97
Judismar Arpini Junior <solid.judis@gmail.com>
2025-07-04 11:56:08 -0300

    Create solucao1.py

---
+++ 2025-07-04/solucao1.py
@@ -0,0 +1,10 @@
+S, A, B = int(input()), int(input()), int(input())
+cont = 0
+for num in range(A, B+1):
+    num = str(num)
+    l = []
+    for digito in num:
+        l.append(int(digito))
+    if sum(l) == S:
+        cont += 1
+print(cont)
