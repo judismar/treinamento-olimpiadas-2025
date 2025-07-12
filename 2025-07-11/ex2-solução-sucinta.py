988a2a7353bca3ad155ce08960baf3654e502d98
Judismar Arpini Junior <solid.judis@gmail.com>
2025-07-12 19:55:44 -0300

    Add files via upload

---
+++ 2025-07-11/ex2-solução-sucinta.py
@@ -0,0 +1,6 @@
+dicionarioCifras = {'a': 'a', 'b': 'bac', 'c': 'cad', 'd': 'def', 'e': 'e', 'f': 'feg', 'g': 'geh', 'h': 'hij', 'i': 'i', 'j': 'jik', 'k': 'kil', 'l': 'lim', 'm': 'mon', 'n': 'nop', 'o': 'o', 'p': 'poq', 'q': 'qor', 'r': 'ros', 's': 'sut', 't': 'tuv', 'u': 'u', 'v': 'vux', 'x': 'xuz', 'z': 'zuz'}
+palavra = input()
+cifrada = ""
+for letra in palavra:
+        cifrada += dicionarioCifras[letra]
+print(cifrada)
